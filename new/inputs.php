1eac53d7ec1a042af83cf45e6402c1396b5eb5a1
Alex Jones <contact@purpledonutgames.com>
2013-11-01 10:34:20 -0700

    added inputs for a new page

---
+++ new/inputs.php
@@ -0,0 +1,14 @@
+
+<div class="inputs">
+	<form method="post" action="process.php" enctype="multpart-formdata">
+		<label for="title">Title: </label><input type="text" name="title"/>
+		<label for="location">Location: </label><input type="text" name="location"/>
+		<label for="content">Content: </label><textarea cols="100" rows="10" name= "content"></textarea><br/>OR<br/>
+		<label for="contentfile">Content file: </label><input type="file" name="contentfile"/>
+
+	</form>
+</div>
+
+<?php
+
+?>
